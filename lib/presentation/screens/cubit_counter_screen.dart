538496ddcc1d7d7df4e1a5a1d2859ffceed1fdb0
dcavadia <dcavadia@uninorte.edu.co>
2024-04-24 10:54:41 -0500

    Use and consumption the counter cubit

--- lib/presentation/screens/cubit_counter_screen.dart
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forms_app/presentation/blocs/counter_cubit/counter_cubit.dart';
 
 class CubitCounterScreen extends StatelessWidget {
   const CubitCounterScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => CounterCubit(), child: const _CubitCounterView());
+  }
+}
+
+class _CubitCounterView extends StatelessWidget {
+  const _CubitCounterView();
+
+  @override
+  Widget build(BuildContext context) {
+    final counterState = context.watch<CounterCubit>().state;
+
     return Scaffold(
         appBar: AppBar(
-          title: Text('Cubit Counter'),
+          title: Text('Cubit Counter: ${counterState.transactionCount}'),
           actions: [
             IconButton(
                 onPressed: () => {}, icon: const Icon(Icons.refresh_rounded))
           ],
         ),
         body: Center(
-          child: const Text('Counter value: xx'),
+          child: BlocBuilder<CounterCubit, CounterState>(
+            // buildWhen: (previous, current) => current.counter != previous.counter,
+            builder: (context, state) {
+              return Text('Counter value: ${state.counter}');
+            },
+          ),
         ),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
